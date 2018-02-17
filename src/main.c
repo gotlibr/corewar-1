104c94cc2ecb2a25cd109ba79b49691b8fae55a4
Mykola VUKOLOV <mvukolov@e1r1p17.unit.ua>
2018-02-18 01:40:37 +0200

    minor mistake

--- src/main.c
+++ src/main.c
@@ -1,13 +1,14 @@
 #include "environment.h"
 #include "../libft/classes_lib/linked_list.h"
 #include "../libft/classes_lib/new.h"
+#include "../libft/classes_lib/classes_lib.h"
 
 int 	main(int argc, char **param)
 {
 	static t_environment env;
 
-	void	make_classes(void);
-	if (init_game(param, &env))
+	make_classes();
+	if (!init_game(param, &env))
 		ft_printf_last_error();
 	vm_loop(&env);
 	print_winner(&env);
