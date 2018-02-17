33f743a24e9b0c53d486143417ba51b55878c3cc
Mykola VUKOLOV <mvukolov@e1r1p17.unit.ua>
2018-02-18 00:09:11 +0200

    game manager functions

--- src/loop_manager.h
+++ src/loop_manager.h
@@ -2,4 +2,7 @@
 
 #include "environment.h"
 
-void 	check_live_executions(t_environment *env);
+int		processes_are_dead(t_environment *env);
+void 	update_live_executions(t_environment *env);
+int		wasnt_decreased_since_last(t_environment *env);
+int 	is_bigger_delta_cycle(t_environment *env);
